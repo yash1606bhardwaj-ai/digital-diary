da076c825016004dfa5c4426678b24c2b0ced101
yash1606bhardwaj-ai <yash1606bhardwaj@gmail.com>
2025-09-26 10:08:48 +0530

    Create blockchain.py

---
+++ blockchain.py
@@ -0,0 +1,46 @@
+import hashlib
+import datetime
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.compute_hash()
+
+    def compute_hash(self):
+        block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, str(datetime.datetime.now()), "Genesis Block", "0")
+        self.chain.append(genesis_block)
+
+    def get_last_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        last_block = self.get_last_block()
+        new_block = Block(
+            index=last_block.index + 1,
+            timestamp=str(datetime.datetime.now()),
+            data=data,
+            previous_hash=last_block.hash
+        )
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            curr = self.chain[i]
+            prev = self.chain[i - 1]
+            if curr.hash != curr.compute_hash():
+                return False
+            if curr.previous_hash != prev.hash:
+                return False
+        return True
